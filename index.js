8902d9271f26533b7cf2fb316221b70340b8bf84
ChoogleGrome <aquifjawad@outlook.com.au>
2019-10-25 12:41:44 +1030

    fixxed o_p eq.

--- index.js
+++ index.js
@@ -1,25 +1,12 @@
 function main () {
     var n = document.getElementById("n").value 
 
-    var i_p = n * (1/2 * (Math.sin((360/n) * (Math.PI/180))))
-    var o_p = n * (1/2 * (2/(Math.cos(2* ((360/n) * (Math.PI/180))) + 1) * (Math.sin((360/n) * (Math.PI/180)))))
+    var i_p = n * ((1/2) * (Math.sin((360/n) * (Math.PI/180))))
+    var o_p = n * ((1/2) * (2/(Math.cos(2* ((180/n) * (Math.PI/180))) + 1) * (Math.sin((360/n) * (Math.PI/180)))))
 
     console.log ("Number of Sides: " + n)
     console.log(i_p + " < " + Math.PI + " < " + o_p)
 
     document.getElementById("num_p").innerHTML = "Number of Sides: " + n
     document.getElementById("ineq").innerHTML = i_p + " < " + Math.PI + " < " + o_p
-
-    // var a = document.createElement("a")
-    // var br = document.createElement("br")
-    // var a_text = document.createTextNode("Number of Sides (n): " + n)
-    // var div = document.getElementById("inequalities")
-    // a.appendChild(a_text)
-    // div.appendChild(a)
-    // div.appendChild(br)
-
-    // a = document.createElement("a")
-    // a_text = document.createTextNode(i_p + " < " + Math.PI + " < " + o_p)
-    // a.appendChild(a_text)
-    // div.appendChild(a)
 }
